367371daf4cbdbadab4122792a9615954c965515
Samarskyi <samgeka@gmail.com>
2021-06-29 17:18:54 +0300

    Added RAW_REQUESTS in RateLimitType

--- src/main/java/com/binance/api/client/domain/general/RateLimitType.java
+++ src/main/java/com/binance/api/client/domain/general/RateLimitType.java
@@ -2,14 +2,15 @@ package com.binance.api.client.domain.general;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 /**
  * Rate limiters.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public enum RateLimitType {
 
-	REQUEST_WEIGHT, ORDERS, REQUESTS
+	REQUEST_WEIGHT,
+	ORDERS,
+	REQUESTS,
+	RAW_REQUESTS
 
 }
